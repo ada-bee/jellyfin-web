a46db2e9fab94d155b71c3ef07e73bd954df1237
ada-bee <92441391+ada-bee@users.noreply.github.com>
2024-05-29 16:15:24 +0200

    added js button

--- src/components/homesections/sections/libraryButtons.ts
+++ src/components/homesections/sections/libraryButtons.ts
@@ -22,7 +22,7 @@ function getLibraryButtonsHtml(items: BaseItemDto[]) {
     }
 
     // jellyseer link
-    html += '<a href="https://request.tagliatelle.tv/" target="_blank" class="raised homeLibraryButton jellyseerButton"><span class="material-icons homeLibraryIcon ' + '" aria-hidden="true"></span><span class="homeLibraryText">' + "Requests 🡕" + '</span></a>';
+    html += '<a href="https://request.tagliatelle.tv/" target="_blank" class="raised homeLibraryButton emby-button jellyseerButton"><span class="material-icons homeLibraryIcon ' + 'view_list' + '" aria-hidden="true"></span><span class="homeLibraryText">' + "Requests 🡕" + '</span></a>';
 
     html += '</div>';
     html += '</div>';
